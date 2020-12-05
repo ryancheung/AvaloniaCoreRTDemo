3776f778d08a2775bd6d75596df8099751551703
ryancheung <ryancheung.go@gmail.com>
2020-12-05 20:50:06 +0800

    WIP

--- src/Program.cs
+++ src/Program.cs
@@ -2,7 +2,6 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Logging.Serilog;
 
 namespace AvaloniaCoreRTDemo
 {
@@ -18,6 +17,6 @@ namespace AvaloniaCoreRTDemo
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
-                .LogToDebug();
+                .LogToTrace();
     }
 }
